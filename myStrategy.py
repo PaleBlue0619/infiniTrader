d1e8c48c6434e15aef98c102b24a5e0cd217cf4c
Maxim_ZJU <2072323503@qq.com>
2026-05-14 16:41:08 +0800

    first commit

---
+++ myStrategy.py
@@ -0,0 +1,132 @@
+from typing import Literal
+
+# 从 base 库中导入定义参数和状态映射模型必须的三个方法
+from pythongo.base import BaseParams, BaseState, Field, BaseStrategy
+from pythongo.classdef import KLineData, OrderData, TickData, TradeData
+from pythongo.core import KLineStyleType
+from pythongo.utils import KLineGenerator
+
+class Params(BaseParams):
+    """参数映射模型 -> 从无限易窗口中传入的参数定义的值
+    Field: 自定义元数据->添加至参数映射模型的字段中
+    default: 定义这个参数的默认值
+    title: 定义这个参数的中文明 -> 会在PythonGO中显示
+    """
+    exchange: str = Field(default="", title="交易所代码")
+    instrument_id: str = Field(default="", title="合约代码")
+    order_price: int | float = Field(default=0, title="报单价格")
+    order_volume: int = Field(default=1, title="报单手数")
+    order_direction: Literal["buy", "sell"] = Field(default="buy", title="报单方向")
+
+class State(BaseState):
+    """
+    状态映射模型 -> 在无限易状态栏查看报单编号的值
+    """
+    order_id: int | None = Field(default=None, title="报单编号")
+
+class interMultiStrategy(BaseStrategy):
+    """实盘策略主体
+    在编写回调函数时, 回调函数应当按照以下顺序定义, 用不到的回调函数允许不定义
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        self.formatDict = {
+            "AP": ["CZCE", 3], # 交易所 # 是否省略2
+            "CF": ["CZCE", 3],
+            "CJ": ["CZCE", 3],
+            "CY": ["CZCE", 3],
+            "FG": ["CZCE", 3],
+            "IC": ["CFFEX", 4],
+            "IF": ["CFFEX", 4],
+            "IH": ["CFFEX", 4],
+            "IM": ["CFFEX", 4],
+            "JR": ["CZCE", 3],
+            "MA": ["CZCE", 3],
+            "OI": ["CZCE", 3],
+            "PF": ["CZCE", 3],
+            "PK": ["CZCE", 3],
+            "PL": ["CZCE", 3],
+            "PM": ["CZCE", 3],
+            "PR": ["CZCE", 3],
+            "PX": ["CZCE", 3],
+            "RI": ["CZCE", 3],
+            "RM": ["CZCE", 3],
+            "RS": ["CZCE", 3],
+            "SA": ["CZCE", 3],
+            "SF": ["CZCE", 3],
+            "SH": ["CZCE", 3],
+            "SM": ["CZCE", 3],
+            "SR": ["CZCE", 3],
+            "T": ["CFFEX", 4],
+            "TA": ["CZCE", 3],
+            "TF": ["CFFEX", 4],
+            "TL": ["CFFEX", 4],
+            "TS": ["CFFEX", 4],
+            "UR": ["CZCE", 3],
+            "WH": ["CZCE", 3],
+            "ZC": ["CZCE", 3],
+            "a": ["DCE", 4],
+            "ad": ["SHFE", 4],
+            "ag": ["SHFE", 4],
+            "al": ["SHFE", 4],
+            "ao": ["SHFE", 4],
+            "au": ["SHFE", 4],
+            "b": ["DCE", 4],
+            "bb": ["DCE", 4],
+            "bc": ["INE", 4],
+            "br": ["SHFE", 4],
+            "bu": ["SHFE", 4],
+            "bz": ["DCE", 4],
+            "c": ["DCE", 4],
+            "cs": ["DCE", 4],
+            "cu": ["SHFE", 4],
+            "eb": ["DCE", 4],
+            "ec": ["INE", 4],
+            "eg": ["DCE", 4],
+            "fb": ["DCE", 4],
+            "fu": ["SHFE", 4],
+            "hc": ["SHFE", 4],
+            "i": ["DCE", 4],
+            "j": ["DCE", 4],
+            "jd": ["DCE", 4],
+            "jm": ["DCE", 4],
+            "l": ["DCE", 4],
+            "lc": ["GFFEX", 4],
+            "lg": ["DCE", 4],
+            "lh": ["DCE", 4],
+            "lu": ["INE", 4],
+            "m": ["DCE", 4],
+            "ni": ["SHFE", 4],
+            "nr": ["INE", 4],
+            "op": ["SHFE", 4],
+            "p": ["DCE", 4],
+            "pb": ["SHFE", 4],
+            "pd": ["GFEX", 4],
+            "pg": ["DCE", 4],
+            "pp": ["DCE", 4],
+            "ps": ["GFEX", 4],
+            "pt": ["GFEX", 4],
+            "rb": ["SHFE", 4],
+            "rr": ["DCE", 4],
+            "ru": ["SHFE", 4],
+            "sc": ["INE", 4],
+            "si": ["GFEX", 4],
+            "sn": ["SHFE", 4],
+            "sp": ["SHFE", 4],
+            "ss": ["SHFE", 4],
+            "v": ["DCE", 4],
+            "wr": ["SHFE", 4],
+            "y": ["DCE", 4],
+            "zn": ["SHFE", 4]
+        }
+        self.posDict = {}
+        """
+        {"AU2501": {"start_date", "end_date", "price", "static_high", "static_low"}
+        }
+        """
+
+    def on_init(self) -> None:
+        print("init")
+
+
+
